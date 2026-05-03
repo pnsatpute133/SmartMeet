ec8f918a501c3f028225739687c1c6cbacbdca28
pnsatpute133 <64823919+pnsatpute133@users.noreply.github.com>
2026-05-03 21:04:08 +0530

    connected successfully

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,5 @@
-export const BASE_URL = "http://192.168.0.102:5002";
-export const SERVER_URL = "http://192.168.0.102:5002"; // Keeping for backward compatibility
-export const AI_URL = "http://192.168.0.102:8000";
+const hostname = window.location.hostname;
+
+export const BASE_URL = `http://${hostname}:5002`;
+export const SERVER_URL = `http://${hostname}:5002`; // Keeping for backward compatibility
+export const AI_URL = `http://${hostname}:8000`;
